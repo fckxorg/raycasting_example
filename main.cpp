e13c9da996614795316a6562590289e72fd116b5
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2020-10-21 01:37:26 +0300

    created diffused lighting

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <SFML/System/Vector2.hpp>
 #include <SFML/Window/VideoMode.hpp>
 #include <math.h>
+#include <unistd.h>
 
 class Vector3 {
    public:
@@ -14,7 +15,16 @@ class Vector3 {
     Vector3(int x, int y, int z)
         : x(x), y(y), z(z) {}
     ~Vector3() = default;
+    
+    float abs() {
+        return sqrt(x * x + y * y + z * z);
+    }
 };
+
+int operator^(const Vector3& first, const Vector3& second) {
+    return first.x * second.x + first.y * second.y + first.z * second.z;
+}
+
 
 void place_pixel(sf::Image& img, sf::Color color, Vector3 pos) {
     pos.x += img.getSize().x / 2;
@@ -22,12 +32,18 @@ void place_pixel(sf::Image& img, sf::Color color, Vector3 pos) {
     img.setPixel(pos.x, pos.y, color);
 }
 
-void draw_circle(sf::Image& image, int radius, Vector3 pos) {
+void draw_circle(sf::Image& image, int radius, Vector3 pos, Vector3 lighting_pos) {
     for (int i = pos.x - radius; i < pos.x + radius; ++i) {
         for (int j = pos.y - radius; j < pos.y + radius; ++j) {
             if (i * i + j * j <= radius * radius) {
                 int z = sqrt(radius* radius - i * i - j * j);
-                sf::Color color((static_cast<float>(z) / radius) * 255, 0, 0);
+                
+                Vector3 sphere_point = Vector3(i, j, z);
+
+                float intensity = std::max(0.f, (sphere_point ^ lighting_pos) / (sphere_point.abs() * lighting_pos.abs())); 
+                printf("Intensity %f\n", intensity);
+
+                sf::Color color(intensity * 255, 0, 0);
                 place_pixel(image, color, Vector3(i, j, z));
             }
         }
@@ -42,19 +58,23 @@ int main() {
     image.create(800, 800, sf::Color::Black);
     image_texture.create(800, 800);
 
-    draw_circle(image, 200, Vector3(0, 0, 0));
-    image_texture.loadFromImage(image);
-    image_sprite.setTexture(image_texture);
 
     window.clear(sf::Color::Black);
+    int i = 0;
     while (window.isOpen()) {
+        draw_circle(image, 200, Vector3(0, 0, 0), Vector3(-300, -300, i));
+        image_texture.loadFromImage(image);
+        image_sprite.setTexture(image_texture);
         window.draw(image_sprite);
+
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) window.close();
         }
 
         window.display();
+        sleep(1);
+        i += 50;
     }
     return 0;
 }
